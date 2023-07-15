dcc3051e285ff8f9289c7915d27bc7f045164790
Anwara Favour <favourchika02@gmail.com>
2023-07-15 14:04:14 +0100

    first commit

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,77 @@
+import { useLogin } from "@refinedev/core";
+import { useEffect, useRef } from "react";
+
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import { yariga } from '../assets';
+
+import { CredentialResponse } from "../interfaces/google";
+
+
+export const Login: React.FC = () => {
+  const { mutate: login } = useLogin<CredentialResponse>();
+
+  const GoogleButton = (): JSX.Element => {
+    const divRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+      if (typeof window === "undefined" || !window.google || !divRef.current) {
+        return;
+      }
+
+      try {
+        window.google.accounts.id.initialize({
+          ux_mode: "popup",
+          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+          callback: async (res: CredentialResponse) => {
+            if (res.credential) {
+              login(res);
+            }
+          },
+        });
+        window.google.accounts.id.renderButton(divRef.current, {
+          theme: "filled_blue",
+          size: "medium",
+          type: "standard",
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }, []);
+
+    return <div ref={divRef} />;
+  };
+
+  return (
+   
+
+      
+    <Container
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        display="flex"
+        gap="36px"
+        justifyContent="center"
+        flexDirection="column"
+      >
+       <Typography align="center" color={"text.secondary"} fontSize="12px">
+          <img
+            alt="Yariga logo"
+            src={yariga}
+          />
+        </Typography>
+
+        <GoogleButton />
+
+        
+      </Box>
+    </Container>
+  );
+};
